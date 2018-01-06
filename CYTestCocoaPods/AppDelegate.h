32b9b71f034afeabe81fcb47ae84e21df899465c
chenyu <chenyu3@gomeholdings.com>
2018-01-06 22:04:47 +0800

    Initial Commit

---
+++ CYTestCocoaPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CYTestCocoaPods
+//
+//  Created by chenyu on 2018/1/6.
+//  Copyright © 2018年 chenyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
